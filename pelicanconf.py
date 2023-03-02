c02603ccf540480fe8d6bfbbfc9608be82ac6f66
Kirill Ephanov <kirill.ephanov@gmail.com>
2023-03-02 13:51:22 +0300

    First commit

---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+AUTHOR = 'Kirill Ephanov'
+SITENAME = 'kirephanov Blog'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Moscow'
+
+DEFAULT_LANG = 'ru'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),)
+
+# Social widget
+SOCIAL = (('kirephanov', 'https://github.com/kirephanov'),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
